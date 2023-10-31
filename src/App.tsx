c98e3b7a81307140594ec33ec80ac5db952fb9f9
Dcose <17873001707@163.com>
2023-10-31 18:02:14 +0800

    🦄 refactor: homePage...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import HomePage from './views/HomePage'
-import Button from './component/button/Button'
-// import '@/app.scss'
 
 function App() {
   return (
     <div>
       <HomePage />
-      <Button />
     </div>
   )
 }
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import styles from './Header.module.scss'
 
 function HomePage() {
   return (
     <div>
-      <header style={{ height: '80px' }}>Header</header>
-      <main>Main</main>
-      <footer>Footer</footer>
+      <header className={styles.header}>
+        <div>slow</div>
+        <nav>
+          <ul className={styles.nav}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/products">Products</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="login-cart-section">login</div>
+      </header>
+
+      <main className={styles.main}>
+        <h2>s1ow</h2>
+        <p>What kind of world do you want to shape ?</p>
+      </main>
+      <footer className={styles.footer}>Footer</footer>
     </div>
   )
 }
