65141f78e1ffa01a113f1453d206410dd868a205
Dcose <17873001707@163.com>
2023-11-10 11:18:57 +0800

    ✨ feat: progress~

--- src/component/navbar/NavLink.tsx
+++ src/component/navbar/NavLink.tsx
@@ -1,36 +1,11 @@
 import React from 'react'
 
+import { colorVariants } from '@/style/color'
+
 interface NavLinkProps {
   href: string
   color?: string
   children: React.ReactNode
-}
-
-const colorVariants: {
-  [key: string]: string
-} = {
-  slate: 'after:bg-slate-500',
-  gray: 'after:bg-gray-500',
-  zinc: 'after:bg-zinc-500',
-  neutral: 'after:bg-neutral-50',
-  stone: 'after:bg-stone-500',
-  red: 'after:bg-red-500',
-  orange: 'after:bg-orange-500',
-  amber: 'after:bg-amber-500',
-  yellow: 'after:bg-yellow-500',
-  lime: 'after:bg-lime-500',
-  green: 'after:bg-green-500',
-  emerald: 'after:bg-emerald-500',
-  teal: 'after:bg-teal-500',
-  cyan: 'after:bg-cyan-500',
-  sky: 'after:bg-sky-500',
-  blue: 'after:bg-blue-500',
-  indigo: 'after:bg-indigo-500',
-  violet: 'after:bg-violet-500',
-  purple: 'after:bg-purple-500',
-  fuchsia: 'after:bg-fuchsia-500',
-  pink: 'after:bg-pink-500',
-  rose: 'after:bg-rose-500'
 }
 
 const NavLink: React.FC<NavLinkProps> = ({
---
+++ src/component/progress/Progress.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useScroll, useSpring, motion } from 'framer-motion'
+
+import { colors } from '@/style/color'
+
+interface ProgressProps {
+  color?: string
+}
+
+const Progress: React.FC<ProgressProps> = ({ color = 'neutral' }) => {
+  console.log('%c Line:12 🥟 color', 'color:#42b983', color)
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  })
+
+  return (
+    <motion.div
+      className={`fixed top-0 left-0 right-0 h-1 ${colors[color]}
+      transform origin-left`}
+      style={{ scaleX }}
+    />
+  )
+}
+
+export default Progress
